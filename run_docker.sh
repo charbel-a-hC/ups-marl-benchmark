ed5ce2d6168df4483b657517ee7b3536da9b3ce1
Charbel Abi Hana <charbel-a-h@outlook.com>
2022-05-24 02:53:57 +0200

    Added GPU Support when launching Docker Container

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 
-docker run -it --rm -v ${PWD}:/ups-marl-benchmark -p 8888:8888 ups-marl-benchmark
+docker run -it --rm --gpus=all -v ${PWD}:/ups-marl-benchmark -p 8888:8888 ups-marl-benchmark
